bd3f90817f67abf8a1331214d79450ca0c38c990
RarelyCharlie <RarelyCharlie@users.noreply.github.com>
2018-04-14 16:45:46 +0200

    Create glossary.js

---
+++ assets/glossary.js
@@ -0,0 +1,18 @@
+// framed glossary support, runs in parent window
+rc_wait = null
+rc_cleanup = null
+
+rc_keypress = function () {
+	if (rc_wait) clearTimeout(rc_wait)
+	rc_wait = setTimeout(rc_gloss, 850)
+	if (rc_cleanup) {
+		clearTimeout(rc_cleanup)
+		rc_cleanup = null
+		}
+	return event.keyCode != 13
+	}
+ 
+rc_gloss = function () {
+  console.log('rc_gloss')
+  }
+  
