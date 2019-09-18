94f0026d8b41910963c8122b0e8368524501ccbb
RarelyCharlie <RarelyCharlie@users.noreply.github.com>
2019-09-18 14:00:48 +0100

    linkify

--- _includes/forummap.js
+++ _includes/forummap.js
@@ -1,7 +1,7 @@
 $(function () {
   let h = ''
   for (let c of data) {
-    h += '<h3>' + c.title + '</h3>'
+    h += '<h3><a href="https://' + c.url + '">' + c.title + '</a></h3>'
     }
   $('#map').html(h)
   })
