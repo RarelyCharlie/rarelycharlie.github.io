af065c6072c49d6e9fb580c2c1d1fd86abfa38ee
RarelyCharlie <RarelyCharlie@users.noreply.github.com>
2019-09-18 14:06:57 +0100

    typo

--- _includes/forummap.js
+++ _includes/forummap.js
@@ -3,7 +3,7 @@ $(function () {
   let h = ''
   for (let c of data) {
     h += '<h3><a href="' + base + c.url + '">' + c.title + '</a></h3>'
-    if (c.strap) h += '<p>' + c.strap + '</p>
+    if (c.strap) h += '<p>' + c.strap + '</p>'
     }
   $('#map').html(h)
   })
