785159622e0673da3375caa76284d09fd6b650ad
RarelyCharlie <RarelyCharlie@users.noreply.github.com>
2019-09-18 14:05:16 +0100

    show strap

--- _includes/forummap.js
+++ _includes/forummap.js
@@ -1,7 +1,9 @@
 $(function () {
+  const base = 'https://www.7cups.com'
   let h = ''
   for (let c of data) {
-    h += '<h3><a href="https://' + c.url + '">' + c.title + '</a></h3>'
+    h += '<h3><a href="' + base + c.url + '">' + c.title + '</a></h3>'
+    if (c.strap) h += '<p>' + c.strap + '</p>
     }
   $('#map').html(h)
   })
